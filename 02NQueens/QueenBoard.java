68844c29e6f529fcaa50d3bf625aa14d2e0391e9
Alex Lu <alexlu876@gmail.com>
2017-02-08 12:33:00 -0500

    initial

---
+++ 02NQueens/QueenBoard.java
@@ -0,0 +1,48 @@
+public class QueenBoard{
+    private int[][] board;
+    private int solutionCount;
+    
+    public QueenBoard(int size){
+	board = new int[size][size];
+	for(int x = 0; x < board.length; x++){
+	    for(int y = 0; y < board.length; y++){
+		board[x][y] = 0;
+	    }
+	}
+    }
+
+    /**
+     *precondition: board is filled with 0's only.
+     *@return false when the board is not solveable. true otherwise.
+     *postcondition: 
+     *if false: board is still filled with 0's
+     *if true: board is filled with the 
+     *final configuration of the board after adding 
+     *all n queens. Uses solveH
+     */
+   
+    public int availability(){
+	int temp = 0;
+	for(int x = 0; x < board.length; x++){
+	    for(int y = 0; y < board.length; y++){
+		if
+    public boolean solve(){
+	
+    }
+
+    /**
+     *@return the number of solutions found, or -1 if the board was never solved.
+     *The board should be reset after this is run.    
+     */
+    public int getSolutionCount(){
+    	return -1;
+    }
+    /**toString
+     *and all nunbers that represent queens are replaced with 'Q' 
+     *all others are displayed as underscores '_'
+     */
+    public String toString(){
+    	return "";
+    }
+}public class Recursion{
+    
