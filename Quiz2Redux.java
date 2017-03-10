75c794a248fef713ba61073af02eb607403d61dd
Alex Lu <alexlu876@gmail.com>
2017-03-10 11:57:16 -0500

    quiz finished

---
+++ Quiz2Redux.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Quiz2Redux{  
+  /*Returns an ArrayList<String> that contains all subsets of the
+   *characters of String s. Assume s has no duplicate characters.
+   *the characters should appear in the same order that they occur 
+   *in the original string.
+   */
+  public static ArrayList<String> combinations(String s){
+      ArrayList<String>words = new ArrayList<String>();
+      help(s, words, 0, "");
+      Collections.sort(words);
+      System.out.println(words);
+      Collections.sort(words);
+      return words;
+  }
+  
+    private static void help(String s, ArrayList<String> words, int index, String current){
+	if(words.indexOf(current) == -1){
+	    words.add(current);
+	}
+	if(index == s.length()){
+	    return;
+	}
+	help(s, words, index + 1, current);
+	help(s, words, index + 1, current + s.charAt(index));
+  }
+
+    public static void main(String[] a){
+	combinations("wxof");
+    }
+	
+}
