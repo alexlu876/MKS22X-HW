ea6646aaad1072166cad44d68dfb1c33fdf879f8
Alex Lu <alexlu876@gmail.com>
2017-03-23 12:29:25 -0400

    finally works

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -4,12 +4,12 @@ public class Quick{
     public Quick(){
     }
     
-    public void part(int[] a, int start, int end){
+    public int[] part(int[] a, int start, int end){
 	Random r = new Random();
 	int pivot = r.nextInt(end - start) + start;
 	int value = a[pivot];
-	System.out.println(pivot);
-	System.out.println(value);
+	//System.out.println(pivot);
+	//System.out.println(value);
 	int i = start;
 	int lt = start;
 	int gt = end;
@@ -27,6 +27,32 @@ public class Quick{
 		gt--;
 	    }
 	}
+	int[] temp = new int[2];
+	temp[0] = lt;
+	temp[1] = gt;
+	//System.out.println(Arrays.toString(temp));
+	//System.out.println(Arrays.toString(a));
+	return temp;
+    }
+
+    public void quicksort(int[] a){
+	quickHelp(a, 0, a.length - 1);
+    }
+
+    public void quickHelp(int[] a, int start, int end){
+	if(start >= end){
+	    return;
+	}
+	int[] temp = part(a, start, end);
+	int low = temp[0];
+	int high = temp[1];
+	quickHelp(a, start, low - 1);
+	quickHelp(a, high + 1, end);	
+    }
+
+    public int quickSelect(int[] a, int k){
+	quicksort(a);
+	return a[k];
     }
 
     public void swap(int[] a, int b, int c){
@@ -37,9 +63,9 @@ public class Quick{
 
     public static void main(String[] args){
 	Quick q = new Quick();
-	int[] test = new int[]{5, 2, 3, 7, 9, 1, 4};
-	q.part(test, 0, 6);
-	System.out.println(Arrays.toString(test));
+	int[] test = new int[]{999, 999, 999, 0, 5, 5, 5, 4, 3, 7, 9, 6, 999, 999, 999, 3, 1, 10, 234123, 23};
+	q.quicksort(test);
+	//System.out.println(Arrays.toString(test));
     }
 }
 	
